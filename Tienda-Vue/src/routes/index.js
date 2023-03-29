b62b1850b94c5777bf4054abee370bf89a772b5a
AxelTorresRuiz <71788346+AxelTorresRuiz@users.noreply.github.com>
2023-03-29 11:15:55 -0600

    Creacion del vue pedidos

--- Tienda-Vue/src/routes/index.js
+++ Tienda-Vue/src/routes/index.js
@@ -3,12 +3,15 @@ import HomeComponent from '@/views/admin/home/HomeComponent'
 import SettingsComponent from '@/views/admin/settings/SettingsComponent'
 import NotFoundComponent from '@/views/admin/notfound/NotFoundComponent'
 import ProductsComponent from '@/views/admin/products/ProductsComponent'
+import PedidosComponent from '@/views/admin/pedidos/PedidosComponent'
 
 const routes =[
     {path:'/admin',name:'admin',component:HomeComponent},
     {path:'/admin/settings',name:'settings',component:SettingsComponent},
     {path:'/admin/products',name:'products',component:ProductsComponent},
+    {path:'/admin/pedidos',name:'pedidos',component:PedidosComponent},
     {path:'/:pathMatch(.*)*',name:"notfound",component:NotFoundComponent}
+    
     
 ]
 const router =vueRouter.createRouter({
---
+++ Tienda-Vue/src/views/admin/pedidos/PedidosComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <HeaderComponent/>
+    <div class="row">
+        <aside class="col-3">
+            <SidebarComponent/>
+            <!--END SIDEBAR-->
+        </aside>
+        <main class="col-10">
+            
+        </main>
+        </div>
+</template>
+<script>
+    import HeaderComponent from '@/components/layouts/HeaderComponent.vue'
+    import SidebarComponent from '@/components/layouts/SidebarComponent.vue'
+    export default{
+        name:'PedidosComponent',
+        components:{
+            HeaderComponent,
+            SidebarComponent
+        }
+    }
+    
+</script>
