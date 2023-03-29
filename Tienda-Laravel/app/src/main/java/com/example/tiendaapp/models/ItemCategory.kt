4ad7854a084bf588e88d3c95f1c1776082bb9ef7
AxelTorresRuiz <71788346+AxelTorresRuiz@users.noreply.github.com>
2023-03-29 09:33:23 -0600

    Correccion

--- Tienda-Laravel/app/src/main/java/com/example/tiendaapp/models/ItemCategory.kt
+++
@@ -1,3 +0,0 @@
-package com.example.tiendaapp.models
-
-class ItemCategory (val id:Number, val nombre:String)
--- Tienda-Laravel/app/src/main/java/com/example/tiendaapp/models/ItemProduct.kt
+++
@@ -1,3 +0,0 @@
-package com.example.tiendaapp.models
-
-data class ItemProduct(val id:Number,val name:String,val price:Double, val img:String, val like:Boolean)
